b6e486b6a0a6643e7ded998384166f08688af43a
Ed.Zhang <71359408+EzNoGame@users.noreply.github.com>
2024-11-08 11:36:05 -0500

    Update CourseProject.h

--- CourseProject.h
+++ CourseProject.h
@@ -4,11 +4,15 @@
 #include <opencv2/opencv.hpp>
 
 
+/// <summary>
+/// this is a test update
+/// </summary>
+/// <returns></returns>
+
 int main() {
 	cv::Mat image = cv::imread("./palacio.png", 1);
 	cv::namedWindow("OpenCV Test", cv::WINDOW_AUTOSIZE);
 	cv::imshow("OpenCV Test", image);
-
 	cv::waitKey(0);
 	return 0;
 }
